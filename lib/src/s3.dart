ded55475c43f6aea03645fc958a35a4e0df5c50d
_kitg <gerasimovkit@gmail.com>
2025-03-09 13:27:30 -0400

    test: empty test doc: licence doc: s3 dartdoc

--- lib/src/s3.dart
+++ lib/src/s3.dart
@@ -51,7 +51,45 @@ Map<String, PreSignedUrl> parseUploadLinks(Map json) {
   };
 }
 
-
+/// Uploads a file to an S3 bucket using pre-signed URL credentials.
+///
+/// This function sends a multipart `POST` request to the specified URL using the form fields
+/// and file data defined in the provided [cred] and [file]. It is designed to work with
+/// pre-signed URLs typically generated for AWS S3 or compatible storage systems.
+///
+/// The function logs the response status, indicating success for HTTP 204 or providing detailed error
+/// messages upon failure.
+///
+/// ### Example:
+/// ```dart
+/// final result = await uploadToBucket(
+///   (url: 'https://example-bucket.s3.amazonaws.com', fields: {'key': 'fileKey'}),
+///   ('fileField', [104, 105, 106], filename: 'example.txt', contentType: 'text/plain'),
+/// );
+/// if (result) {
+///   print('File uploaded successfully');
+/// } else {
+///   print('Upload failed');
+/// }
+/// ```
+///
+/// ### Parameters:
+/// - **[cred]**: A [PreSignedUrl] containing the:
+///   - `url`: The pre-signed URL for the upload endpoint.
+///   - `fields`: The required form fields for the upload request.
+/// - **[file]**: A [MultipartFile] that represents:
+///   - `field`: The field name for the uploaded file.
+///   - `value`: The file content in bytes.
+///   - `filename` (Optional): The file's name on upload.
+///   - `contentType` (Optional): The MIME type of the file, such as `application/pdf` or `image/png`.
+///
+/// ### Returns:
+/// - A `Future<bool>`:
+///   - `true` if the upload is successful (HTTP status code 204).
+///   - `false` for any other status code indicating failure.
+///
+/// ### Throws:
+/// - A `Future.error` if an exception occurs during the HTTP request, such as a network error.
 Future<bool> uploadToBucket(PreSignedUrl cred, MultipartFile file) async {
   final PreSignedUrl(:url, :fields) = cred;
   final request = http.MultipartRequest('POST', Uri.parse(url))
